73f92f990747272b5427f97212a33c42cdae7588
Harendrasinh <haren111990@gmail.com>
2025-03-05 20:49:44 +0530

    font colore change try

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -11,26 +11,26 @@ export const Contact = () => {
           <form>
             <div class="grid gap-6 mb-6 md:grid-cols-2">
               <div>
-                <label for="first_name" class="block mb-1 text-[15px] font-medium text-orange-400 dark:text-white">First name</label>
+                <label for="first_name" class="block mb-1 text-[15px]  font-medium md:text-orange-400 dark:text-white">First name</label>
                 <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Vayu" required />
               </div>
               <div>
-                <label for="phone" class="block mb-1 text-[15px] font-medium text-orange-400 dark:text-white">Phone number</label>
+                <label for="phone" class="block mb-1 text-[15px] font-medium md:text-orange-400 dark:text-white">Phone number</label>
                 <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="9987658982" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
               </div>
             </div>
             <div class="grid gap-6 mb-6 md:grid-cols-2">
               <div>
-                <label for="email" class="block mb-1 text-[15px] font-medium text-orange-400 dark:text-white">Your Email</label>
+                <label for="email" class="block mb-1 text-[15px] font-medium md:text-orange-400 dark:text-white">Your Email</label>
                 <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="haren128@gmail.com" required />
               </div>
               <div>
-                <label for="city" class="block mb-1 text-[15px] font-medium text-orange-400 dark:text-white">City</label>
+                <label for="city" class="block mb-1 text-[15px] font-medium md:text-orange-400 dark:text-white">City</label>
                 <input type="text" id="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your City" required />
               </div>
             </div>
 
-            <label for="message" class="block mb-1 text-[15px] font-medium text-orange-400 dark:text-white">Your message</label>
+            <label for="message" class="block mb-1 text-[15px] font-medium md:text-orange-400 dark:text-white">Your message</label>
             <textarea id="message" rows="4" class="block p-2.5 w-full text-sm mb-2 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your message here..."></textarea>
 
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
