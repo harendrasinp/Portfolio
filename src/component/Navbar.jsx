d9011788bee9dbe060dcdfc1cdfb4f30e2cbcae9
Harendrasinh <haren111990@gmail.com>
2025-03-09 17:55:28 +0530

    update navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react'
 import "./ComponentStyle.css"
 import "./Mediaquery.css"
 import { Download } from './Download'
-import { Link, useLocation } from 'react-router-dom'
+import { NavItems } from "./NavItems"
 
 export const Navbar = () => {
 
@@ -11,25 +10,16 @@ export const Navbar = () => {
       <div className={"navBar"}>
         <div className={"Profile-name-div"} >
           <img src="/myImage.jpg" alt="my-Image" />
-          <div className="detailContainer">
-            <div className={"myName"}>Harendrasinh.R.Parmar</div>
+          <div className="detailContainer ">
             <div className={"detailDiv"}>
-              <p className="text-white font-thin"><span className="font-semibold">Mobile:</span> 9867775626</p>
-              <p className="text-white font-thin"><span className="font-semibold">Email:</span> haren111990@gmail.com</p>
+              <div className='text-[20px] font-semibold text-orange-600'>Harendrasinh.R.Parmar</div>
+              <div className='text-gray-50 font-semibold'>Mobile: 9867775626</div>
+              <div className='text-gray-50 font-semibold'>Email:haren111990@gmail.com</div>
               <hr />
             </div>
           </div>
         </div>
-        <div className={"navItemsDiv"}>
-          <div className={"navbarItem"}>
-            <Link to={"/"}>Home</Link>
-            <Link to={"contact"}>Contact me</Link>
-            <Link to={"/aboutUs"}>About me</Link>
-            <a href="https://github.com/harendrasinp">
-              <span>Github</span><img src="https://cdn-icons-png.flaticon.com/128/11376/11376339.png" alt="github" />
-            </a>
-          </div>
-        </div>
+        <NavItems/>
       </div>
       <Download />
     </div>
