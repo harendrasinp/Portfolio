88d55028e21a670557c061a2c48ad22d061d3bb6
Harendrasinh <haren111990@gmail.com>
2025-03-10 13:49:44 +0530

    UpdatedPdf

--- src/component/Download.jsx
+++ src/component/Download.jsx
@@ -3,7 +3,7 @@ export const Download= () => {
   return (
     <div>
           <div className="bg-orange-400 px-4 py-2 md:px-2 md:py-1 flex gap-2 items-center rounded-md ">
-            <a className='text-xl ' href="/pdf/Resume/HarendrasinhResume2025.pdf" target="_blank">Download</a>
+            <a className='text-xl ' href="/pdf/Resume/Harendrasinh-Resume2025.pdf" target="_blank">Download</a>
             <img className='w-[1.25rem] h-[1.25rem]' src="https://cdn-icons-png.flaticon.com/128/7268/7268609.png" alt="downlaod" />
           </div>
     </div>
