197f3bca4c2a129e828030bffa8678f6ace80876
Harendrasinh <haren111990@gmail.com>
2025-04-05 23:25:30 +0530

    add carosal

--- src/component/MyCarousel.jsx
+++ src/component/MyCarousel.jsx
@@ -7,16 +7,27 @@ export const MyCarousel = () => {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToShow:1,
+        slidesToScroll: 1,
     };
     return (
-        <div className=' w-[15rem]'>
+        <div className=' w-[18rem]'>
             <Slider {...settings}>
-                <div><img className='w-[15rem] h-[10rem]' src="../public/background.jpg" alt="Image 1" /></div>
-                <div><img className='w-[15rem] h-[10rem]' src="../public/myImage.jpg" alt="Image 2" /></div>
-                <div><img className='w-[15rem] h-[10rem]' src="../public/background.jpg" alt="Image 3" /></div>
+            <div><img className='w-[20rem] h-[10rem]'src="../public/certificate_Image/backend1.jpg"alt="Image 1"/>
+            </div>
+            <div><img className='w-[20rem] h-[10rem]'src="../public/certificate_Image/backend2.jpg"alt="Image 1"/>
+            </div>
+            <div><img className='w-[20rem] h-[10rem]'src="../public/certificate_Image/react1.jpg"alt="Image 1"/>
+            </div>
+            <div><img className='w-[20rem] h-[10rem]'src="../public/certificate_Image/react2.jpg"alt="Image 1"/>
+            </div>
+            <div><img className='w-[20rem] h-[10rem]'src="../public/certificate_Image/frontend1.jpg"alt="Image 1"/>
+            </div>
+            <div><img className='w-[20rem] h-[10rem]'src="../public/certificate_Image/frontend2.jpg"alt="Image 1"/>
+            </div>
+                
             </Slider>
         </div>
     )
 }
+
